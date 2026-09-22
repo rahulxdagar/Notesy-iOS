e73f8f89ea296135e29c0d2f029008295786751a
Rahul <rahulxdagar@gmail.com>
2026-09-22 15:08:53 -0400

    Initial Commit

---
+++ Notesy/NotesyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesyApp.swift
+//  Notesy
+//
+//  Created by Rahul Dagar on 2026-09-22.
+//
+
+import SwiftUI
+
+@main
+struct NotesyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
